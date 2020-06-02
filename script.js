48d8b82d717747ddc3dfa05821cd7f8fc018e931
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-02 14:08:28 +0200

    commented

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ w1 = Number((Math.random() * 100).toFixed(0));
 w2 = Number((Math.random() * 100).toFixed(0));
 b0 = Number((Math.random() * 100).toFixed(0));
 
-console.log(w1, w2, b0);
+// console.log(w1, w2, b0);
 
 // Computes simple perceptron output
 function computePerceptronOutput(x1, x2, w1, w2, b0) {
