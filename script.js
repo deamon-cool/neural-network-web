4a64867859acee44bcd6bf773ce8ec90c93ab19c
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-02 12:35:43 +0200

    corrected

--- script.js
+++ script.js
@@ -42,20 +42,24 @@ let b0 = random();
 // console.log(w1, w2, w3, w4, w5, w6, b1, b2, b0);
 
 
-//-------------------------- Single perceptron
+//-------------------------- Single perceptron: 2 inputs, one output
 
-console.log(w1, w2, b);
+w1 = Number((Math.random() * 100).toFixed(0));
+w2 = Number((Math.random() * 100).toFixed(0));
+b0 = Number((Math.random() * 100).toFixed(0));
 
-// Simple perceptron
-function computePerceptronOutput(x1, x2, w1, w2, b) {
-    let output = x1 * w1 + x2 * w2 + b;
+console.log(w1, w2, b0);
+
+// Computes simple perceptron output
+function computePerceptronOutput(x1, x2, w1, w2, b0) {
+    let output = x1 * w1 + x2 * w2 + b0;
 
     return output;
 }
 
-// Compute corrected weight and bias
-function computeParamameters(x1, x2, d, y) {
-    let n = 0.01;
+// Computes corrected weight and bias
+function computeParameters(x1, x2, d, y) {
+    let n = 1;
     let diff = d - y;
 
     w1 += n * diff * x1;
